ca44b23a87fc1a355e75b1e414763c3c0deeaf84
RDemarch <robin.demarch@sfr.fr>
2020-05-12 15:48:23 +0200

    BG About

--- index.php
+++ index.php
@@ -43,9 +43,9 @@
     </section>
   </div>
   <section class="about container-fluid">
-      <div class="w-50 vh-100 bg-about"></div>
+      <div class="container-fluid w-50 h-100 bg-about"></div>
       <div class="about-part">
-        <div class="about-profile w-50 h-100">
+        <div class="about-profile w-50 ">
             <header>
               <h3 class="font-montserrat-bold uppercase part">About</h3>
               <h1 class="title font-librebaskerville-bold">More About Me.</h1>
@@ -53,9 +53,27 @@
                  adipisicing minim ad cupidatat eu veniam nostrud
                  mollit laboris sunt magna velit culpa consectetur
                  nostrud consectetur labore sed do.</p>
-
-
             </header>
+            <section>
+
+              <p>Lorem ipsum Nisi officia Duis irure voluptate dolor commodo
+                 pariatur occaecat aliquip adipisicing voluptate Ut in qui ea
+                 sint occaecat in commodo in in in incididunt ut sunt in Ut Duis
+                 in ut ex qui anim cupidatat cupidatat ex in non dolore labore ea
+                 amet cillum ea qui dolor nisi sed velit mollit exercitation ex fugiat
+                 labore in deserunt culpa laborum culpa anim dolore laboris amet irure
+                 mollit proident velit fugiat aute ea elit magna consequat qui officia
+                 quis elit Duis dolor esse cupidatat tempor proident voluptate aliqua ex
+                 cupidatat do eiusmod veniam irure laborum ut magna nostrud dolore ullamco
+                 commodo elit sit magna aliqua laborum veniam officia dolor.</p>
+
+                <p>Lorem ipsum Minim nisi dolore proident Ut dolor quis deserunt voluptate
+                   Ut voluptate enim ut adipisicing fugiat cupidatat nisi adipisicing labore
+                  non adipisicing in sit proident. Ut in qui ea sint occaecat in commodo in
+                  in in incididunt ut</p>
+
+
+            </section>
 
         </div>
         <div class="about-service">
